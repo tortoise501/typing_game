049c75fbaaecc53c359d66febb57afe8ddabdaad
tortoise501 <tralala5.555.d@gmail.com>
2024-01-26 17:49:17 +0100

    matching by letters instead of words

--- src/gameio/output.rs
+++ src/gameio/output.rs
@@ -1,19 +1,24 @@
-use crate::{TextStatus,TypedString};
 use crossterm::cursor::MoveTo;
 use crossterm::style::{Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor};
 use crossterm::execute;
 use crossterm::terminal::{Clear, ClearType};
 use std::io::stdout;
 
+use crate::{LetterState,Letter};
 
-pub fn print_game_text(text_vec:&Vec<TypedString>) -> std::io::Result<&'static str>{
+
+pub fn print_game_text(text_vec:&Vec<Letter>) -> std::io::Result<&'static str>{
     // using the macro
-    let print_as = |text:&TypedString| -> std::io::Result<&'static str> {
-
-        let foreground_color = match text.text_status {
-            TextStatus::Unfilled => Color::Grey,
-            TextStatus::Filled => Color::Green,
-            TextStatus::Wrong => Color::Red,
+    execute!{
+        stdout(),
+        Clear(ClearType::All),
+        MoveTo(10, 10),//testing
+    }?;
+    for letter in text_vec{
+        let foreground_color = match letter.state {
+            LetterState::Unfilled => Color::Grey,
+            LetterState::Correct => Color::Green,
+            LetterState::Wrong => Color::Red,
         };
         let background_color = Color::Black;
 
@@ -21,20 +26,10 @@ pub fn print_game_text(text_vec:&Vec<TypedString>) -> std::io::Result<&'static s
             stdout(),
             SetForegroundColor(foreground_color),
             SetBackgroundColor(background_color),
-            Print(text.text.as_str()),
+            Print(letter.c),
             ResetColor
         )?;
-        Ok("ok")
-    };
-    execute!{
-        stdout(),
-        Clear(ClearType::All),
-        MoveTo(10, 10),//testing
-    }?;
-    for one_status_string in text_vec{
-        print_as(&one_status_string)?;
     }
-    
     
     Ok("ok")
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+
+
 extern crate crossterm;
 
 mod gameio;
@@ -22,7 +24,7 @@ fn start_game(){
                 }
             },
         }
-        let matched_text = get_matched_strings(&written_text, &created_text);
+        let matched_text = get_matched_letter_vec(&created_text,&written_text);
         match gameio::output::print_game_text(&matched_text) {
             Ok(msg) => _ = msg,
             Err(msg) => panic!("Error in output function, {}",msg)
@@ -38,124 +40,72 @@ fn get_random_test() -> String {
     String::from("Not a random text, only used for testing")
 }
 
-fn get_matched_strings(written_text:&String, created_text:&String) -> Vec<TypedString> {
-    let written_text:Vec<char> = written_text.chars().collect();
-    let created_text:Vec<char> = created_text.chars().collect();
+fn get_matched_letter_vec(correct_string: &String, written_string: &String) -> Vec<Letter>{
+    let mut res: Vec<Letter> = Vec::new();
+    let correct_string: Vec<char> = correct_string.chars().collect();
+    let written_string: Vec<char> = written_string.chars().collect();
 
-    let mut res:Vec<TypedString> = Vec::new();
+    if written_string.len() > correct_string.len(){
+        panic!("written text bigger than correct text");
+    }
 
-    let mut last_text_status = None;
-    let mut one_status_string = String::new();
     
-    let mut push_new_status_string = |c: &char, status: TextStatus, new: bool| {
-        if new{
-            let test = TypedString{
-                text_status: status,
-                text: one_status_string.clone(),
-            };
-            res.push(test);
-            one_status_string = String::new();
-        }
-        one_status_string.push(c.clone());
-    };
 
-    let mut compare_for = |c: &char, status:TextStatus|{
-        
 
-        let last_text_status_some = match last_text_status {
-            Some(st) => st,
-            None => TextStatus::Unfilled,
-        };
-        match last_text_status_some as i32 * status as i32 {
-            1 => {
-                push_new_status_string(c,last_text_status_some,false)
-            },
-            -1  => {
-                push_new_status_string(c,last_text_status_some,true);
-                last_text_status = Some(status);
-            },
-            0 => {
-                let do_new = match last_text_status_some {
-                    TextStatus::Unfilled => false,
-                    _ => true
-                };
-                push_new_status_string(c,last_text_status_some,do_new);
-                last_text_status = Some(status)
-            },
-            _ => todo!()
-        }
-    };
-
-    for (i, c1) in created_text.iter().enumerate(){
-        if written_text.len() <= i{
-            compare_for(c1,TextStatus::Unfilled);
+    for (i,c) in correct_string.iter().enumerate(){
+        if i >= written_string.len(){
+            res.push(Letter {c:c.clone(),state:LetterState::Unfilled});
             continue;
         }
-        let c2 = &written_text[i];
-        if c1 == c2 {
-            compare_for(c1,TextStatus::Filled);
-        }else{
-            compare_for(c1,TextStatus::Wrong);
-        }
+        let letter_state: LetterState = match c {
+            c if c == &written_string[i] => LetterState::Correct,
+            _ => LetterState::Wrong,
+        };
+        res.push(Letter {c:c.clone(),state:letter_state});
     }
-    res.push(TypedString{
-        text_status: match last_text_status {
-            Some(st) => st,
-            None => TextStatus::Unfilled,
-        },
-        text: one_status_string.clone(),
-    });
+
     res
 }
 
+#[derive(Debug)]
+#[derive(PartialEq)]
+struct Letter{
+    c: char,
+    state: LetterState
+}
+#[derive(PartialEq)]
+#[derive(Debug)]
+enum LetterState {
+    Unfilled,
+    Correct,
+    Wrong
+}
 #[cfg(test)]
 mod test{
     use super::*;
     #[test]
-    fn matching_fully(){
-        let test = get_matched_strings(&String::from("the cake is a lie"), &String::from("the cake is a lie"));
-        assert_eq!(test,vec![TypedString{text_status:TextStatus::Filled,text:String::from("the cake is a lie")}]);
+    fn match_all_correct(){
+        let test = get_matched_letter_vec(&String::from("ccccc"), &String::from("ccccc"));
+        let answer = vec![Letter{c:'c',state: LetterState::Correct},Letter{c:'c',state: LetterState::Correct},Letter{c:'c',state: LetterState::Correct},Letter{c:'c',state: LetterState::Correct},Letter{c:'c',state: LetterState::Correct},];
+        assert_eq!(test,answer);
     }
     #[test]
-    fn not_matching_fully(){
-        let test = get_matched_strings(
-            &String::from("the many shall suffer for the sins of the one"),
-            &String::from("i like pierogis and herbata,-----------------"));
-        assert_eq!(test,vec![TypedString{text_status:TextStatus::Wrong,text:String::from("i like pierogis and herbata,-----------------")},]);
+    fn match_all_wrong(){
+        let test = get_matched_letter_vec(
+            &String::from("wwwww"), 
+            &String::from("-----")
+        );
+        let answer = vec![Letter{c:'w',state: LetterState::Wrong},Letter{c:'w',state: LetterState::Wrong},Letter{c:'w',state: LetterState::Wrong},Letter{c:'w',state: LetterState::Wrong},Letter{c:'w',state: LetterState::Wrong},];
+        assert_eq!(test,answer);
     }
     #[test]
-    fn matching_not_fully(){
-        let test = get_matched_strings(
-            &String::from("This is filled string,wrong chars only, but this is"), 
-            &String::from("This is filled string, and this isn't , but this is,this is unfilled")
+    fn match_mixed(){
+        let test = get_matched_letter_vec(
+            &String::from("ccwcu"), 
+            &String::from("cc-c")
         );
-        assert_eq!(
-            test,
-            vec![
-                TypedString{text_status:TextStatus::Filled,text:String::from("This is filled string,")},
-                TypedString{text_status:TextStatus::Wrong,text:String::from(" and this isn't ")},
-                TypedString{text_status:TextStatus::Filled,text:String::from(", but this is")},
-                TypedString{text_status:TextStatus::Unfilled,text:String::from(",this is unfilled")},
-                ]
-        );
+        let answer = vec![Letter{c:'c',state: LetterState::Correct},Letter{c:'c',state: LetterState::Correct},Letter{c:'w',state: LetterState::Wrong},Letter{c:'c',state: LetterState::Correct},Letter{c:'u',state: LetterState::Unfilled},];
+        assert_eq!(test,answer);
     }
-}
-
-
-
-#[derive(Clone)]
-#[derive(PartialEq)]
-#[derive(Debug)]
-enum TextStatus{
-    Unfilled = 0,
-    Filled = 1,
-    Wrong = -1
-}
-impl Copy for TextStatus {
-}
-#[derive(PartialEq)]
-#[derive(Debug)]
-struct TypedString{
-    text_status: TextStatus,
-    text: String,
+    
 }
