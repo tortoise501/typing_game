3e0e1738842a46b567186ad722115176c7226d66
tortoise501 <tralala5.555.d@gmail.com>
2024-02-11 20:58:43 +0100

    statistics window and controls

--- src/component/statistic_component.rs
+++ src/component/statistic_component.rs
@@ -8,10 +8,36 @@ pub struct StatComp {
 #[allow(dead_code)]
 impl Component for StatComp {
     fn handle_message(&mut self, msg: Message) -> Message {
-        todo!()
+        let answer = match msg {
+            Message::PressedKey(code) => match code {
+                KeyCode::Esc => Some(Message::Quit),
+                KeyCode::Enter => Some(Message::GoToWindow(WindowType::Menu(MenuComp))),
+                _ => None,
+            },
+            _ => None,
+        };
+        match answer {
+            Some(a) => a,
+            None => msg,
+        }
     }
 
     fn view(&mut self, f: &mut Frame) {
-        todo!()
+        f.render_widget(
+            Paragraph::new(format!(
+                "Your speed: {} wpm. At the end of the test you had {} mistakes.\nPress 'Enter' to go to menu or 'Esc' to exit the game.",
+                2,
+                self.game
+                    .written_vec
+                    .iter()
+                    .filter(|letter| { letter.state == LetterState::Wrong })
+                    .count()
+            ))
+            .block(Block::new().title("Paragraph").borders(Borders::ALL))
+            .style(Style::new().white().on_black())
+            .alignment(Alignment::Left)
+            .wrap(Wrap { trim: false }),
+            f.size(),
+        );
     }
 }
--- src/game.rs
+++ src/game.rs
@@ -1,41 +1,58 @@
-#[derive(Clone)]
-#[derive(Debug)]
+#[derive(Clone, Debug)]
 pub struct Game {
     pub correct_text: Vec<char>,
     pub written_vec: Vec<Letter>,
 }
 impl Game {
-    pub fn new() -> Game{
+    pub fn new() -> Game {
         Game {
-            correct_text: Game::get_random_test(4).chars().collect(),
+            correct_text: Game::get_random_text(5).chars().collect(),
             written_vec: Vec::new(),
         }
     }
-    pub fn char_key_pressed(&mut self,c:char){
+
+    /// "Press" char key for written text
+    pub fn char_key_pressed(&mut self, c: char) {
         if self.correct_text[self.written_vec.len()] == c {
-            self.written_vec.push(Letter { c, state: LetterState::Correct });
+            self.written_vec.push(Letter {
+                c,
+                state: LetterState::Correct,
+            });
         } else {
-            self.written_vec.push(Letter { c: self.correct_text[self.written_vec.len()].clone(), state: LetterState::Wrong });
+            self.written_vec.push(Letter {
+                c: self.correct_text[self.written_vec.len()].clone(),
+                state: LetterState::Wrong,
+            });
         }
     }
 
+    /// Returns true if length of written text is the same as length of correct text
     pub fn is_complete(&self) -> bool {
         self.written_vec.len() == self.correct_text.len()
     }
 
-    pub fn backspace_pressed(&mut self){
+    /// "Press" backspace for written text
+    pub fn backspace_pressed(&mut self) {
         self.written_vec.pop();
     }
 
-    pub fn get_written_vec(&mut self) -> Vec<Letter>{
+    /// Gets vector of letters including unfilled letters from correct text
+    /// Used only for rendering
+    ///
+    ///
+    pub fn get_written_vec(&mut self) -> Vec<Letter> {
         let mut res = self.written_vec.clone();
-        for c in &self.correct_text[self.written_vec.len()..]{
-            res.push(Letter{ c:c.clone(), state: LetterState::Unfilled });
+        for c in &self.correct_text[self.written_vec.len()..] {
+            res.push(Letter {
+                c: c.clone(),
+                state: LetterState::Unfilled,
+            });
         }
         res
     }
 
-    pub fn get_random_test(size:i32) -> String {
+    ///Gets random text for a game
+    pub fn get_random_text(size: usize) -> String {
         //String::from("Ryan Thomas Gosling is a Canadian actor. Prominent in both independent film and major studio features of varying genres, his films have accrued a worldwide box office gross of over 1.9 billion USD. He has received various accolades, including a Golden Globe Award, as well as nominations for three Academy Awards and two BAFTAs. ")
         let mut chain = markov::Chain::new();
         let er = chain.feed_file("text_for_markov.txt");
@@ -43,34 +60,29 @@ impl Game {
             Ok(_) => (),
             Err(e) => panic!("{e}"),
         }
-        String::from_iter(chain.str_iter_for(size as usize))
+        let a = &chain.generate_str()[..size];
+        String::from(a)
     }
 }
 
-
-#[derive(Debug)]
-#[derive(PartialEq)]
-#[derive(Clone)]
-pub struct Letter{
+#[derive(Debug, PartialEq, Clone)]
+pub struct Letter {
     pub c: char,
-    pub state: LetterState
+    pub state: LetterState,
 }
-#[derive(PartialEq)]
-#[derive(Debug)]
-#[derive(Clone)]
+#[derive(PartialEq, Debug, Clone)]
 pub enum LetterState {
     Unfilled,
     Correct,
-    Wrong
+    Wrong,
 }
 
-
 #[cfg(test)]
-mod test{
+mod test {
     use super::*;
     #[test]
-    fn match_all_correct(){
-        let mut test_game = Game{
+    fn match_all_correct() {
+        let mut test_game = Game {
             correct_text: "ccccc".chars().collect(),
             written_vec: Vec::new(),
         };
@@ -78,13 +90,34 @@ mod test{
         for c in written_text {
             test_game.char_key_pressed(c);
         }
-        let answer = vec![Letter{c:'c',state: LetterState::Correct},Letter{c:'c',state: LetterState::Correct},Letter{c:'c',state: LetterState::Correct},Letter{c:'c',state: LetterState::Correct},Letter{c:'c',state: LetterState::Correct},];
-        assert_eq!(test_game.get_written_vec(),answer);
+        let answer = vec![
+            Letter {
+                c: 'c',
+                state: LetterState::Correct,
+            },
+            Letter {
+                c: 'c',
+                state: LetterState::Correct,
+            },
+            Letter {
+                c: 'c',
+                state: LetterState::Correct,
+            },
+            Letter {
+                c: 'c',
+                state: LetterState::Correct,
+            },
+            Letter {
+                c: 'c',
+                state: LetterState::Correct,
+            },
+        ];
+        assert_eq!(test_game.get_written_vec(), answer);
     }
 
     #[test]
-    fn match_all_wrong(){
-        let mut test_game = Game{
+    fn match_all_wrong() {
+        let mut test_game = Game {
             correct_text: "wwwww".chars().collect(),
             written_vec: Vec::new(),
         };
@@ -92,13 +125,34 @@ mod test{
         for c in written_text {
             test_game.char_key_pressed(c);
         }
-        let answer = vec![Letter{c:'w',state: LetterState::Wrong},Letter{c:'w',state: LetterState::Wrong},Letter{c:'w',state: LetterState::Wrong},Letter{c:'w',state: LetterState::Wrong},Letter{c:'w',state: LetterState::Wrong},];
-        assert_eq!(test_game.get_written_vec(),answer);
+        let answer = vec![
+            Letter {
+                c: 'w',
+                state: LetterState::Wrong,
+            },
+            Letter {
+                c: 'w',
+                state: LetterState::Wrong,
+            },
+            Letter {
+                c: 'w',
+                state: LetterState::Wrong,
+            },
+            Letter {
+                c: 'w',
+                state: LetterState::Wrong,
+            },
+            Letter {
+                c: 'w',
+                state: LetterState::Wrong,
+            },
+        ];
+        assert_eq!(test_game.get_written_vec(), answer);
     }
 
     #[test]
-    fn match_mixed(){
-        let mut test_game = Game{
+    fn match_mixed() {
+        let mut test_game = Game {
             correct_text: "ccwcu".chars().collect(),
             written_vec: Vec::new(),
         };
@@ -106,8 +160,28 @@ mod test{
         for c in written_text {
             test_game.char_key_pressed(c);
         }
-        let answer = vec![Letter{c:'c',state: LetterState::Correct},Letter{c:'c',state: LetterState::Correct},Letter{c:'w',state: LetterState::Wrong},Letter{c:'c',state: LetterState::Correct},Letter{c:'u',state: LetterState::Unfilled},];
-        assert_eq!(test_game.get_written_vec(),answer);
+        let answer = vec![
+            Letter {
+                c: 'c',
+                state: LetterState::Correct,
+            },
+            Letter {
+                c: 'c',
+                state: LetterState::Correct,
+            },
+            Letter {
+                c: 'w',
+                state: LetterState::Wrong,
+            },
+            Letter {
+                c: 'c',
+                state: LetterState::Correct,
+            },
+            Letter {
+                c: 'u',
+                state: LetterState::Unfilled,
+            },
+        ];
+        assert_eq!(test_game.get_written_vec(), answer);
     }
-    
 }
