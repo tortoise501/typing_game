acd468f06d6f6df31ba118647d90b421f0c713f4
tortoise501 <tralala5.555.d@gmail.com>
2024-02-16 19:53:22 +0100

    working rewrite game mode

--- src/component/game_component.rs
+++ src/component/game_component.rs
@@ -1,3 +1,5 @@
+use ratatui::layout::{self, Constraint, Direction, Layout};
+
 use super::*;
 
 #[derive(Debug)]
@@ -31,6 +33,14 @@ impl Component for GameComp {
     }
 
     fn view(&mut self, f: &mut Frame) {
+        match self.game.game_mode {
+            crate::game::GameMode::Normal => self.normal_view(f),
+            crate::game::GameMode::Rewrite => self.rewrite_view(f),
+        }
+    }
+}
+impl GameComp {
+    fn normal_view(&mut self, f: &mut Frame) {
         let matched_letter_vec = self.game.get_written_vec();
 
         let mut text: Vec<Span> = Vec::new();
@@ -67,6 +77,66 @@ impl Component for GameComp {
                 .alignment(Alignment::Left)
                 .wrap(Wrap { trim: false }),
             f.size(),
+        );
+    }
+    fn rewrite_view(&mut self, f: &mut Frame) {
+        let matched_letter_vec = self.game.get_written_vec();
+
+        let mut text: Vec<Span> = Vec::new();
+        let mut unfilled_started = false;
+        for letter in matched_letter_vec {
+            text.push(Span::styled(
+                format!("{}", letter.c),
+                match letter.state {
+                    FieldState::Unfilled if !unfilled_started => {
+                        unfilled_started = true;
+                        Style::new().on_gray().gray().not_underlined() //cursor
+                    }
+                    FieldState::Unfilled => break,
+                    FieldState::Correct => Style::new().green().not_underlined(),
+                    FieldState::Wrong => {
+                        if letter.c == ' ' {
+                            Style::new()
+                                .red()
+                                .underlined()
+                                .underline_color(ratatui::style::Color::Red)
+                        } else {
+                            Style::new().red().not_underlined()
+                        }
+                    }
+                },
+            ));
+        }
+        let text: Line = Line::from(text);
+
+        let layout = Layout::new(
+            Direction::Vertical,
+            [Constraint::Percentage(50), Constraint::Percentage(50)],
+        )
+        .split(f.size());
+
+        f.render_widget(
+            Paragraph::new(text)
+                .block(Block::new().title("Paragraph").borders(Borders::ALL))
+                .style(Style::new().white().on_black())
+                .alignment(Alignment::Left)
+                .wrap(Wrap { trim: false }),
+            layout[0],
+        );
+        f.render_widget(
+            Paragraph::new(
+                self.game
+                    .correct_text
+                    .iter()
+                    .map(|x| x.to_string())
+                    .collect::<Vec<String>>()
+                    .concat(),
+            )
+            .block(Block::new().title("Paragraph").borders(Borders::ALL))
+            .style(Style::new().white().on_black())
+            .alignment(Alignment::Left)
+            .wrap(Wrap { trim: false }),
+            layout[1],
         );
     }
 }
