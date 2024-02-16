27c74892e5d9e0d2db402c165c43afcae8aba1e1
tortoise501 <tralala5.555.d@gmail.com>
2024-02-16 20:31:06 +0100

    temporary fix for Ctrl + Backspace, can be fully fixed with new message 'CtrlBackspace' and fixing input functions to return messages

--- src/input.rs
+++ src/input.rs
@@ -1,23 +1,31 @@
-
 use crossterm::event::{read, Event, KeyCode, KeyEvent, KeyModifiers};
 
 use std::io::Result;
 
 use crate::Message;
 
-
 fn try_read_key() -> Result<Option<KeyCode>> {
     // Set terminal to raw mode
     let mut res = Ok(None);
-        // Read next event
-    if let Event::Key(KeyEvent { code, modifiers, kind: _, state: _ }) = read()? {
+    // Read next event
+    if let Event::Key(KeyEvent {
+        code,
+        modifiers,
+        kind: _,
+        state: _,
+    }) = read()?
+    {
         match code {
             KeyCode::Char(c) => {
-                if modifiers == KeyModifiers::CONTROL && c == 'c'{
+                if modifiers == KeyModifiers::CONTROL && c == 'c' {
                     panic!("Ctrl + C");
                 }
-                res =  Ok(Some(code));
-            },
+                if modifiers == KeyModifiers::CONTROL && (c == 'w' || c == 'h') {
+                    res = Ok(Some(KeyCode::Backspace));
+                } else {
+                    res = Ok(Some(code));
+                }
+            }
             _ => {
                 res = Ok(Some(code));
             }
@@ -26,20 +34,16 @@ fn try_read_key() -> Result<Option<KeyCode>> {
     res
 }
 
-pub fn get_input_message() -> Option<Message>{
+pub fn get_input_message() -> Option<Message> {
     let res = try_read_key();
     match res {
-        Ok(Some(code)) => {
-            Some(Message::PressedKey(code))
-        },
-        Ok(None) =>{
-            None
-        },
+        Ok(Some(code)) => Some(Message::PressedKey(code)),
+        Ok(None) => None,
         Err(e) => {
             if e.to_string() == "Ctrl + c" {
                 Some(Message::Quit)
             } else {
-                panic!("Error during input processing: {} ",e)
+                panic!("Error during input processing: {} ", e)
             }
         }
     }
